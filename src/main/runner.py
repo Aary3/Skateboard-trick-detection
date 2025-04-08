3a8d322ab723c1c45656b97783ea3db4b4b3fcf6
Piotr Sosnowski <piotrsosnowski@autograf.pl>
2025-04-08 20:42:35 +0200

    Added data frame creation for video output

--- src/main/runner.py
+++ src/main/runner.py
@@ -23,6 +23,45 @@ def main():
         # Perform detection on the video
         results = detector.detect(path_to_video)
 
+        # Print the results
+        if modelType == "pose":
+            oldDataFrame = None
+            for result in results:
+                if ((result.keypoints.xy is None) or (result.keypoints.conf is None)):
+                    continue                                #skip if no keypoints are detected
+                keypoints = result.keypoints.xy.cpu().numpy()
+                keypointsconf = result.keypoints.conf.cpu().numpy()
+                i = 1
+                for keypoint in keypoints:                  #keypoints is a list of numpy arrays, each array is a list of keypoints
+                    tempDataFrame = pd.DataFrame(keypoint, columns=["x", "y"])
+                    tempDataFrame["confidence"] = keypointsconf[i-1]
+                    if i == 1:
+                        poseDataFrame = tempDataFrame
+                    else:
+                        poseDataFrame = pd.concat([poseDataFrame, tempDataFrame], axis=0)
+                    i += 1
+                if oldDataFrame is None:
+                    oldDataFrame = poseDataFrame.copy()
+                else:
+                    oldDataFrame = pd.concat([oldDataFrame, poseDataFrame], axis=0)
+            poseDataFrame = oldDataFrame.copy()
+            poseDataFrame = poseDataFrame.reset_index(drop=True)
+            print(poseDataFrame)
+        else:
+            frame = 1
+            for result in results:
+                if frame == 1:
+                    positionDataFrame = pd.DataFrame(result.boxes.xyxy.cpu().numpy(), columns=["x1", "y1", "x2", "y2"])
+                    positionDataFrame["confidence"] = result.boxes.conf.cpu().numpy()
+                    positionDataFrame["class"] = result.boxes.cls.cpu().numpy()
+                else:
+                    tempDataFrame = pd.DataFrame(result.boxes.xyxy.cpu().numpy(), columns=["x1", "y1", "x2", "y2"])
+                    tempDataFrame["confidence"] = result.boxes.conf.cpu().numpy()
+                    tempDataFrame["class"] = result.boxes.cls.cpu().numpy()
+                    positionDataFrame = pd.concat([positionDataFrame, tempDataFrame], axis=0)
+                frame += 1
+            print(positionDataFrame)    
+
         # Create a detectionVisualizer instance and visualize the video
         visualizer = detectionVisualizer(results=results)
         visualizer.visualizeVideo(path_to_video)
@@ -36,8 +75,6 @@ def main():
 
         # Print the results
         if modelType == "pose":
-            print("Pose Detection Results:")
-            print(results[0].keypoints.xy)                  #keypoints in xy format
             keypoints = results[0].keypoints.xy.cpu().numpy()
             keypointsconf = results[0].keypoints.conf.cpu().numpy()
             i = 1
@@ -51,8 +88,6 @@ def main():
                 i += 1
             print(poseDataFrame)
         else:
-            print("Object Detection Results:")
-            print(results[0].boxes.xyxy)                    #bounding boxes in xyxy format
             positionDataFrame = pd.DataFrame(results[0].boxes.xyxy.cpu().numpy(), columns=["x1", "y1", "x2", "y2"])
             positionDataFrame["confidence"] = results[0].boxes.conf.cpu().numpy()
             positionDataFrame["class"] = results[0].boxes.cls.cpu().numpy()
